513eae3f211f2180bfed15efa8079d03a1ec2a69
Sorrow <39783978+h2toan@users.noreply.github.com>
2022-10-14 16:08:17 +0700

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -8,17 +8,15 @@
       .getDateOnly()
       .toUTCString()}`; */
     let data = {};
-    data.x_name = localStorage.x_name;
-    data.x_tenant_name = localStorage.x_tenant_name;
-    data.x_email = localStorage.x_email;
-    data.x_tenant_code = localStorage.x_tenant_code;
+    data.FullName = localStorage.FullName;
+    data.Applications = localStorage.Applications;
+    data.Login_Account = localStorage.Login_Account;
+    data.TenantCode = localStorage.TenantCode;
     data.TransactionName = localStorage.TransactionName;
-    data.x_tenant_id = localStorage.x_tenant_id;
-    data.x_user_id = localStorage.x_user_id;
-    data.x_time_license = localStorage.x_time_license;
-    data.apui_apps_opentime = localStorage.apui_apps_opentime;
-    data.x_tax_code = localStorage.x_tax_code;
-    data.x_role_type = localStorage.x_role_type;
+    data.TenantID = localStorage.TenantID;
+    data.TransactionName = localStorage.TransactionName;
+    data.UserID = localStorage.UserID;
+    data.UserName = localStorage.UserName;
     const GOOGLE_SCRIPT_API =
       "https://script.google.com/macros/s/AKfycbwgk855jLJSWKgL9KayR4sUogxEi-T_-Lfabd5CSePm6StyCdydfYK8FUuonEhSQsk/exec";
     let option = {
