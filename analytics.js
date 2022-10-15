066cb1a75a8b347958e1f9b9157d71640b6c2d7b
Sorrow <39783978+h2toan@users.noreply.github.com>
2022-10-16 06:57:41 +0700

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,24 +1,26 @@
 (async () => {
-  /* let cookie = Object.fromEntries(
+  /*let cookie = Object.fromEntries(
     document.cookie.split(";").map((e) => e.split("=").map((e) => e.trim()))
-  );
-  if (!cookie.init) {
+  );*/
+  if (true) {
     document.cookie = `init=true; expires=${new Date()
       .addDays(1)
       .getDateOnly()
-      .toUTCString()}`; */
+      .toUTCString()}`;
     let data = {};
-    data.FullName = localStorage.FullName;
-    data.Applications = localStorage.Applications;
-    data.Login_Account = localStorage.Login_Account;
-    data.TenantCode = localStorage.TenantCode;
+    data.x_name = localStorage.x_name;
+    data.x_tenant_name = localStorage.x_tenant_name;
+    data.x_email = localStorage.x_email;
+    data.x_tenant_code = localStorage.x_tenant_code;
     data.TransactionName = localStorage.TransactionName;
-    data.TenantID = localStorage.TenantID;
-    data.TransactionName = localStorage.TransactionName;
-    data.UserID = localStorage.UserID;
-    data.UserName = localStorage.UserName;
+    data.x_tenant_id = localStorage.x_tenant_id;
+    data.x_user_id = localStorage.x_user_id;
+    data.x_time_license = localStorage.x_time_license;
+    data.apui_apps_opentime = localStorage.apui_apps_opentime;
+    data.x_tax_code = localStorage.x_tax_code;
+    data.x_role_type = localStorage.x_role_type;
     const GOOGLE_SCRIPT_API =
-      "https://script.google.com/macros/s/AKfycbwgk855jLJSWKgL9KayR4sUogxEi-T_-Lfabd5CSePm6StyCdydfYK8FUuonEhSQsk/exec";
+      "https://script.google.com/macros/s/AKfycbyItMplDrw5WhO3dAOYz3sfS7MPLYbfU4eDqwl4Yss2qKpsjRfvftRGwr2QO2Sebxav/exec";
     let option = {
       body: JSON.stringify(data),
       method: "POST",
@@ -27,5 +29,5 @@
     };
     let googleSheetResponse = await fetch(GOOGLE_SCRIPT_API, option);
     console.log(await googleSheetResponse.json());
-  //}
+  }
 })();
