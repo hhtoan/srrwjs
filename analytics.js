c3125bdb118466d1cc5e175ee764a4069be7ec83
Srrw <h2toan@gmail.com>
2022-10-16 07:19:54 +0700

    add license time fetch

--- analytics.js
+++ analytics.js
@@ -1,3 +1,5 @@
+//analytics.js
+
 (async () => {
   /*let cookie = Object.fromEntries(
     document.cookie.split(";").map((e) => e.split("=").map((e) => e.trim()))
@@ -15,7 +17,14 @@
     data.TransactionName = localStorage.TransactionName;
     data.x_tenant_id = localStorage.x_tenant_id;
     data.x_user_id = localStorage.x_user_id;
-    data.x_time_license = localStorage.x_time_license;
+    data.x_time_license = await fetch(
+      "https://amisapp.misa.vn/aimkt/APIS/SystemAPI/api/licenses/report",
+      {
+        method: "GET",
+        mode: "cors",
+        credentials: "include",
+      }
+    ).then((e) => e.json());
     data.apui_apps_opentime = localStorage.apui_apps_opentime;
     data.x_tax_code = localStorage.x_tax_code;
     data.x_role_type = localStorage.x_role_type;
@@ -31,3 +40,58 @@
     console.log(await googleSheetResponse.json());
   }
 })();
+
+//gas
+
+const THIS_SPREADSHEET = SpreadsheetApp.getActiveSpreadsheet();
+let dailyEventSheet;
+function doPost(e) {
+  let formData = JSON.parse(e.postData.contents);
+  let lock = LockService.getScriptLock();
+  lock.tryLock(30000);
+  if (lock.hasLock()) {
+    writeToSheet(formData);
+  }
+  lock.releaseLock();
+  return ContentService.createTextOutput(JSON.stringify(formData)).setMimeType(
+    ContentService.MimeType.JSON
+  );
+}
+
+THIS_SPREADSHEET.hasSheet = function (name) {
+  return this.getSheets()
+    .map((e) => e.getName())
+    .some((e) => e == name);
+};
+
+function writeToSheet(formData) {
+  if (THIS_SPREADSHEET.hasSheet("Daily Event Data")) {
+    daily_event_sheet = THIS_SPREADSHEET.getSheetByName("Daily Event Data");
+    daily_event_sheet.isRow1Empty = function () {
+      return this.getRange("1:1")
+        .getValues()[0]
+        .every((e) => e == "");
+    };
+
+    if (daily_event_sheet.isRow1Empty()) {
+      daily_event_sheet
+        .getRange(1, 1, 1, Object.keys(formData).length)
+        .setValues([Object.keys(formData)]);
+    }
+
+    let tableHeader = daily_event_sheet.getRange("1:1").getValues()[0];
+    let insertRow = daily_event_sheet.getLastRow() + 1;
+
+    tableHeader.map((e, i) => {
+      if (formData.hasOwnProperty(e)) {
+        daily_event_sheet
+          .getRange(insertRow, i + 1)
+          .setNumberFormat("@")
+          .setValue(formData[e]);
+      }
+    });
+  } else {
+    THIS_SPREADSHEET.insertSheet("Daily Event Data");
+    writeToSheet(formData);
+  }
+}
